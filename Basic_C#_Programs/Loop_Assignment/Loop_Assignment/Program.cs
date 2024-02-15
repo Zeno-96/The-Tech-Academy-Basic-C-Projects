697c0536bdd0c3cc08c7d8436048cf0d729ce780
Zeno <BMorter10@gmail.com>
2024-02-15 15:01:33 -0800

    Added files for Loop_Assignment

---
+++ Basic_C#_Programs/Loop_Assignment/Loop_Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Loop_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "brandon";
+            bool check = false;
+
+            while (check == false)
+            {
+                Console.WriteLine("Please guess my name:");
+                string guess = Console.ReadLine();
+
+                if (guess == name)
+                {
+                    Console.WriteLine("Correct!");
+                    check = true;
+                    Console.Read();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Sorry, try again.");
+                    check = false;
+                }
+            }
+            
+        }
+    }
+}
