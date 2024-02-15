fc34396ba2e1205ce4e62a191c4a59b648176860
Zeno <BMorter10@gmail.com>
2024-02-15 15:30:50 -0800

    Added files for String_Assignment

---
+++ Basic_C#_Programs/String_Assignment/String_Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            //This takes the 3 strings above and concatenates themm into a single sentence, then waits.
+            string first = "This is a ";
+            string second = "sentence of ";
+            string third = "concatenated strings!";
+            Console.WriteLine(first + second + third);
+            
+            //This section takes our string and makes it uppercase.
+            string lower = "This sentence used to be lowercase.";
+            Console.WriteLine(lower.ToUpper());
+
+            //This section creates a stringbuilder and appends it 3 times. 
+            StringBuilder builder = new StringBuilder("Terry");
+            builder.Append(", ");
+            builder.Append("Chris");
+            builder.Append(", Paul.");
+            Console.WriteLine(builder);
+            Console.Read();
+        }
+    }
+}
